5c6a0075e524772f13741768f79716b34f73756d
sou-xettri <rakeshchhetri420@gmail.com>
2024-01-17 20:32:21 +0545

    update: login page and layout update

--- client/src/app/(auth)/auth/page.tsx
+++ client/src/app/(auth)/auth/page.tsx
@@ -1,99 +1,12 @@
 "use client";
 
+import LoginPage from "@/components/login/Login";
 import React from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 
-const FormSchema = z.object({
-  email: z.string().email(),
-  password: z.string(),
-});
-
-export default function page() {
-  const form = useForm<z.infer<typeof FormSchema>>({
-    resolver: zodResolver(FormSchema),
-    defaultValues: {
-      email: " ",
-      password: " ",
-    },
-  });
-
-  function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(data);
-  }
+export default function AuthPage() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Card className="w-[350px]">
-        <CardHeader />
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="w-3/2 space-y-6 justify-center items-center flex flex-col"
-          >
-            <CardContent>
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        type="email"
-                        placeholder="Email"
-                        className="h-12 text-xl"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        placeholder="Password"
-                        className="h-12 text-xl mt-7"
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </CardContent>
-            <CardFooter className="flex justify-center ">
-              <Button
-                type="submit"
-                variant="outline"
-                className="text-xl pt-5 pb-5 -mt-5 "
-              >
-                Login
-              </Button>
-            </CardFooter>
-          </form>
-        </Form>
-      </Card>
+    <div>
+      <LoginPage />
     </div>
   );
 }
