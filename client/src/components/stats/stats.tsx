b665a831b022aa7b7d8d8dd2900c027570074bd8
sou-xettri <rakeshchhetri420@gmail.com>
2024-01-18 07:38:28 +0545

    refactor: dashboard overview page

---
+++ client/src/components/stats/stats.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { RecentSales } from "../recent-sales";
+import { Overview } from "../overview";
+
+export const Stats = () => {
+  return (
+    <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
+      <Card className="col-span-4">
+        <CardHeader>
+          <CardTitle>Overview</CardTitle>
+        </CardHeader>
+        <CardContent className="pl-2">
+          <Overview />
+        </CardContent>
+      </Card>
+      <Card className="col-span-3">
+        <CardHeader>
+          <CardTitle>Recent Transactions</CardTitle>
+          <CardDescription>You made 265 transactions this month.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <RecentSales />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
