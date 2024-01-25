515ca51ad9c84893486436c12024f2e60c585a88
sou-xettri <rakeshchhetri420@gmail.com>
2024-01-25 14:22:01 +0545

    Update: game page

--- client/src/app/(dashboard)/games/page.tsx
+++ client/src/app/(dashboard)/games/page.tsx
@@ -19,6 +19,50 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { MoreHorizontal, Phone } from "lucide-react";
 
+const data=[
+  {
+    id:1,
+    name:"Fire Kirin",
+    image:"/fk.jpeg",
+  },
+  {
+    id:2,
+    name:"Orion Stars",
+    image:"/os.png",
+  },
+  {
+    id:3,
+    name:"Game Vault",
+    image:"/gv.jpeg",
+  },
+  {
+    id:4,
+    name:"VB-Link",
+    image:"/vblink.jpeg",
+  },
+  {
+    id:5,
+    name:"Milky Way",
+    image:"/milkyway.jpeg",
+  },
+  {
+    id:6,
+    name:"Juwa",
+    image:"/juwa.jpeg",
+  },
+  {
+    id:7,
+    name:"Panda Master",
+    image:"/pm.jpeg",
+    slug:"pm",
+  },
+  {
+    id:8,
+    name:"Ultra Panda",
+    image:"/up.jpeg",
+  },
+]
+
 export default function Employees() {
   return (
     <>
@@ -36,279 +80,53 @@ export default function Employees() {
         </div>
         <div className="flex-1 space-y-4 p-8 pt-6">
           <div className="flex items-center justify-between space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Employees</h2>
+            <h2 className="text-3xl font-bold tracking-tight">Games</h2>
           </div>
           <div className="items-start justify-center gap-6 rounded-lg  md:grid lg:grid-cols-2 xl:grid-cols-4">
-            <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-              <Card>
-                <CardHeader className="items-center relative">
-                    <div className="absolute right-0 top-0">
-                     <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                          onClick={() => console.log("Copy payment ID")}
-                        >
-                          Copy Employee ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View Employee Details</DropdownMenuItem>
-                        <DropdownMenuItem>
-                          Edit Employee Details
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                    </div>
-                  <Avatar className="w-[150px] h-[150px]">
-                    <AvatarImage src="/ms.jpg" alt="Avatar" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center pb-10">
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    Bonnie Green
-                  </h5>
-                  <span className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    Visual Designer
-                  </span>
-                  <span className="flex text-xs text-gray-500 dark:text-gray-400 underline underline-offset-4 items-center">
-                    <Phone className="w-[14px] h-[14px] mr-1" />
-                    9812345678
-                  </span>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-              <Card>
-                <CardHeader className="items-center relative">
-                    <div className="absolute right-0 top-0">
-                    <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                          onClick={() => console.log("Copy payment ID")}
-                        >
-                          Copy Employee ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View Employee Details</DropdownMenuItem>
-                        <DropdownMenuItem>
-                          Edit Employee Details
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                    </div>
-                  <Avatar className="w-[150px] h-[150px]">
-                    <AvatarImage src="/ms.jpg" alt="Avatar" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center pb-10">
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    Bonnie Green
-                  </h5>
-                  <span className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    Visual Designer
-                  </span>
-                  <span className="flex text-xs text-gray-500 dark:text-gray-400 underline underline-offset-4 items-center">
-                    <Phone className="w-[14px] h-[14px] mr-1" />
-                    9812345678
-                  </span>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-              <Card>
-                <CardHeader className="items-center relative">
-                    <div className="absolute right-0 top-0">
-                     <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                          onClick={() => console.log("Copy payment ID")}
-                        >
-                          Copy Employee ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View Employee Details</DropdownMenuItem>
-                        <DropdownMenuItem>
-                          Edit Employee Details
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                    </div>
-                  <Avatar className="w-[150px] h-[150px]">
-                    <AvatarImage src="/ms.jpg" alt="Avatar" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center pb-10">
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    Bonnie Green
-                  </h5>
-                  <span className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    Visual Designer
-                  </span>
-                  <span className="flex text-xs text-gray-500 dark:text-gray-400 underline underline-offset-4 items-center">
-                    <Phone className="w-[14px] h-[14px] mr-1" />
-                    9812345678
-                  </span>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-              <Card>
-                <CardHeader className="items-center relative">
-                    <div className="absolute right-0 top-0">
-                     <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                          onClick={() => console.log("Copy payment ID")}
-                        >
-                          Copy Employee ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View Employee Details</DropdownMenuItem>
-                        <DropdownMenuItem>
-                          Edit Employee Details
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                    </div>
-                  <Avatar className="w-[150px] h-[150px]">
-                    <AvatarImage src="/ms.jpg" alt="Avatar" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center pb-10">
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    Bonnie Green
-                  </h5>
-                  <span className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    Visual Designer
-                  </span>
-                  <span className="flex text-xs text-gray-500 dark:text-gray-400 underline underline-offset-4 items-center">
-                    <Phone className="w-[14px] h-[14px] mr-1" />
-                    9812345678
-                  </span>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-              <Card>
-                <CardHeader className="items-center relative">
-                    <div className="absolute right-0 top-0">
-                     <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                          onClick={() => console.log("Copy payment ID")}
-                        >
-                          Copy Employee ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View Employee Details</DropdownMenuItem>
-                        <DropdownMenuItem>
-                          Edit Employee Details
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                    </div>
-                  <Avatar className="w-[150px] h-[150px]">
-                    <AvatarImage src="/ms.jpg" alt="Avatar" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center pb-10">
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    Bonnie Green
-                  </h5>
-                  <div className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    Visual Designer
-                  </div>
-                  <span className="flex text-xs text-gray-500 dark:text-gray-400 underline underline-offset-4 items-center">
-                    <Phone className="w-[14px] h-[14px] mr-1" />
-                    9812345678
-                  </span>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
-              <Card>
-                <CardHeader className="items-center relative">
-                    <div className="absolute right-0 top-0">
-                     <DropdownMenu>
-                      <DropdownMenuTrigger asChild>
-                        <Button variant="ghost" className="h-8 w-8 p-0">
-                          <span className="sr-only">Open menu</span>
-                          <MoreHorizontal className="h-4 w-4" />
-                        </Button>
-                      </DropdownMenuTrigger>
-                      <DropdownMenuContent align="end">
-                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
-                        <DropdownMenuItem
-                          onClick={() => console.log("Copy payment ID")}
-                        >
-                          Copy Employee ID
-                        </DropdownMenuItem>
-                        <DropdownMenuSeparator />
-                        <DropdownMenuItem>View Employee Details</DropdownMenuItem>
-                        <DropdownMenuItem>
-                          Edit Employee Details
-                        </DropdownMenuItem>
-                      </DropdownMenuContent>
-                    </DropdownMenu>
-                    </div>
-                  <Avatar className="w-[150px] h-[150px]">
-                    <AvatarImage src="/ms.jpg" alt="Avatar" />
-                    <AvatarFallback>CN</AvatarFallback>
-                  </Avatar>
-                </CardHeader>
-                <CardContent className="flex flex-col items-center pb-10">
-                  <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                    Bonnie Green
-                  </h5>
-                  <span className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                    Visual Designer
-                  </span>
-                  <span className="flex text-xs text-gray-500 dark:text-gray-400 underline underline-offset-4 items-center">
-                    <Phone className="w-[14px] h-[14px] mr-1" />
-                    9812345678
-                  </span>
-                </CardContent>
-              </Card>
-            </div>
+            {
+              data.map((item)=>(
+                <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
+                <Card>
+                  <CardHeader className="items-center relative">
+                      <div className="absolute right-0 top-0">
+                       <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                          <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                          </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                          <DropdownMenuItem
+                            onClick={() => console.log("Copy payment ID")}
+                          >
+                            Copy Game Link
+                          </DropdownMenuItem>
+                            <DropdownMenuItem
+                            onClick={() => console.log("Copy payment ID")}
+                          >
+                            Copy Admin Link
+                          </DropdownMenuItem>
+                          <DropdownMenuSeparator />
+                          <DropdownMenuItem>View Game Details</DropdownMenuItem>
+                        </DropdownMenuContent>
+                      </DropdownMenu>
+                      </div>
+                    <Avatar className="w-[150px] h-[150px] rounded-none">
+                      <AvatarImage src={item.image} alt="Avatar"/>
+                      <AvatarFallback>{item.name}</AvatarFallback>
+                    </Avatar>
+                  </CardHeader>
+                  <CardContent className="flex flex-col items-center pb-10">
+                    <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+                      {item.name}
+                    </h5>
+                  </CardContent>
+                </Card>
+              </div>
+              ))
+            }
           </div>
         </div>
       </div>
